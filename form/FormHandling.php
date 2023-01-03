965b6ca25e90d53b65ebaf262991e8e44761bb94
MohamadAli011 <mohamada0112@gmail.com>
2023-01-04 01:08:14 +0700

    second comit

---
+++ form/FormHandling.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<form action="welcome.php" method="post">
+		Nama: <input type="text" name="name"><br>
+		No hp: <input type="text" name="No"><br>
+		<input type="submit">
+	</form>
+</body>
+</html>
---
+++ form/welcome.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	Welcome <?php echo $_POST["name"]; ?><br>
+	Your No hp is: <?php echo $_POST["No"]; ?>
+</body>
+</html>
